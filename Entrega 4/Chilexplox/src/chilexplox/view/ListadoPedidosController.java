9efef0784d29126a4a00a61d26fbfea8de8b002a
Matias Hurtado <mihurtado@uc.cl>
2015-10-22 09:37:44 -0300

    Listado pedidos y clientes

---
+++ Entrega 4/Chilexplox/src/chilexplox/view/ListadoPedidosController.java
@@ -0,0 +1,19 @@
+package chilexplox.view;
+
+import chilexplox.MainApp;
+import javafx.fxml.FXML;
+
+public class ListadoPedidosController {
+	
+	private MainApp mainApp;
+
+	@FXML
+    private void initialize() {
+        
+    }
+	
+	public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+	
+}
