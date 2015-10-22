4a6236186473bb62af24009ee67e03cf268dbd10
Matias Hurtado <mihurtado@uc.cl>
2015-10-22 10:53:55 -0300

    Cambios empleados y nueva función en sistemas

--- Entrega 4/Chilexplox/src/chilexplox/view/ClientesController.java
+++ Entrega 4/Chilexplox/src/chilexplox/view/ClientesController.java
@@ -1,5 +1,19 @@
 package chilexplox.view;
 
+import chilexplox.MainApp;
+import javafx.fxml.FXML;
+
 public class ClientesController {
+	
+	private MainApp mainApp;
+	
+	@FXML
+    private void initialize() {
+        
+    }
+	
+	public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
 
 }
