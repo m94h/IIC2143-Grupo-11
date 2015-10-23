5f54743e3e0e830fac3412384446f8da1632b88c
Benjamin <btibarra@uc.cl>
2015-10-23 00:14:45 -0300

    ListadoCamiones y base para sus tablas

--- Entrega 4/Chilexplox/src/chilexplox/view/ListadoCamionesController.java
+++ Entrega 4/Chilexplox/src/chilexplox/view/ListadoCamionesController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
@@ -44,6 +45,41 @@ public class ListadoCamionesController {
 
 	@FXML
 	private TextField Kms;
+
+	/*
+	 * Tabla camiones esperando y sus columnas
+	 */
+	@FXML
+	private TableView<EsperandoTableModel> tabla_esperando;
+
+	@FXML
+    private TableColumn<EsperandoTableModel, String> patenteColumn;
+    @FXML
+    private TableColumn<EsperandoTableModel, String> estadoColumn;
+    @FXML
+    private TableColumn<EsperandoTableModel, String> capacidadColumn;
+    @FXML
+    private TableColumn<EsperandoTableModel, String> origenColumn;
+
+    /*
+     * Data de los camiones para la tabla
+     */
+	private ObservableList<EsperandoTableModel> camionesData;
+
+	/*
+	 * Tabla camiones esperando y sus columnas
+	 */
+	@FXML
+	private TableView<ListoTableModel> tabla_listo;
+
+	@FXML
+    private TableColumn<ListoTableModel, String> patente2Column;
+    @FXML
+    private TableColumn<ListoTableModel, String> estado2Column;
+    @FXML
+    private TableColumn<ListoTableModel, String> capacidad2Column;
+    @FXML
+    private TableColumn<ListoTableModel, String> destinoColumn;
 
 
 	@FXML
